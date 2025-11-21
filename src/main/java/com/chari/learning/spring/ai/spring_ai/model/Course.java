59b7485dbffaf4caa90a00f59697cf391ed64427
Lakshminarayana Chari <lnchari.adoni@gmail.com>
2025-11-21 19:34:25 +0530

    initial commit

---
+++ src/main/java/com/chari/learning/spring/ai/spring_ai/model/Course.java
@@ -0,0 +1,4 @@
+package com.chari.learning.spring.ai.spring_ai.model;
+
+public record Course(Integer id, String title, String url, String description) {
+}
---
+++ src/main/java/com/chari/learning/spring/ai/spring_ai/service/CourseService.java
@@ -0,0 +1,45 @@
+package com.chari.learning.spring.ai.spring_ai.service;
+
+import com.chari.learning.spring.ai.spring_ai.model.Course;
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+  private static final Logger logger = LoggerFactory.getLogger(CourseService.class);
+  private List<Course> courses = new ArrayList<>();
+
+  private Course getNotFoundCourse(Integer id) {
+    return new Course(-1, String.format("The requested course with id: %d does not exists.", id), "", "");
+  }
+
+  @Tool(name = "get_all_courses", description = "Get all available courses")
+  public List<Course> getAllCourses() {
+//      logger.info("Fetching all courses. Total courses: {}", courses.size());
+      return Collections.unmodifiableList(courses);
+  }
+
+  @Tool(name = "get_course_by_id", description = "Get course details by ID")
+  public Course getCourseById(Integer id) {
+//      logger.info("Fetching course with ID: {}", id);
+      return courses.stream()
+              .filter(course -> course.id().equals(id))
+              .findFirst()
+              .orElse(getNotFoundCourse(id));
+  }
+
+  @PostConstruct
+  public void init() {
+    courses.add(new Course(1, "MCP Server with Spring AI", "https://www.youtube.com/watch?v=w5YVHG1j3Co", "This is a short course on building an MCP server using Spring AI by Dan Vega."));
+    courses.add(new Course(2, "AI/ML Introduction", "https://www.youtube.com/watch?v=jzEr_AqHXgY", "Why Java developers need to know internals of AI/ML by Frank Greco. This covers basics of AI/ML and its importance for Java developers. It is not for building AI/ML models but for understanding how to use them effectively in Java applications."));
+    courses.add(new Course(3, "Verson Control with Intellij", "https://www.youtube.com/watch?v=-S3Q_-b52rA", "This is a tutorial on using version control systems like Git within the IntelliJ IDEA IDE by Dmitriy Smirnov."));
+    courses.add(new Course(4, "As song sung by Bhimsen Joshi", "https://www.youtube.com/watch?v=_tdYY6lUw9g", "Devotional song Bhagyada Lakshmi Baramma sung by the legendary Bhimsen Joshi."));
+//    logger.info("Initialized CourseService with sample courses.");
+  }
+}
